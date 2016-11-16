86a79dd7f3cb38175dac7f8bc48004c1b3c93dbd
Chang Liu <shampliu@gmail.com>
2016-11-15 16:37:46 -0800

    skeleton code

---
+++ skeleton-code/assets/viz-ajax.js
@@ -0,0 +1,7 @@
+// initialize globals
+
+d3.json("assets/data.json", function(error, data) {
+	if (error) return;
+
+	// handle data here
+}
---
+++ skeleton-code/assets/viz.js
@@ -0,0 +1,3 @@
+var dataset = [ 1, 10, 13, 19, 21, 25, 22, 18, 15, 13 ];
+
+// start coding below
